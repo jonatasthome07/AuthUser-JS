5321f81cc1421e276659f822b9b17ba7482d0841
Jônatas <jonatasthome04@gmail.com>
2025-08-09 21:06:06 -0300

    Login form was added

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,9 +4,11 @@ import jwt from "jsonwebtoken"
 import mongoose from "mongoose"
 
 export default class UserController {
+    static async login (req,res){
+        res.render("user/loginForm")
+    }
 
-
-    static async register (req,res){
-        res.render("user/registerForm")
+    static async loginPost (req,res){
+        const {email, password} = req.body
     }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,8 @@ import express from "express"
 import UserController from "../controllers/UserController.js"
 const router = express.Router()
 
-router.get("/", UserController.register)
+router.get("/login", UserController.login)
+router.post("/loginPost", UserController.loginPost)
+router.get("/register", UserController.login)
 
 export default router
