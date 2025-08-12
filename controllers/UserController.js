a58ca09537db912776f7853693e3f1f28a144031
Jônatas <jonatasthome04@gmail.com>
2025-08-11 21:24:40 -0300

    Finished register form validations

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -20,7 +20,46 @@ export default class UserController {
         const {name,email, password,confirmpass} = req.body
         if (!name){
             req.flash("msg", "O nome é um campo obrigatório")
-            return res.redirect("/register",)
+            return res.redirect("/register")
+        }
+        if (!email){
+            req.flash("msg", "O e-mail é um campo obrigatório")
+            return res.redirect("/register")
+        }
+        if (!password){
+            req.flash("msg", "A senha é um campo obrigatório")
+            return res.redirect("/register")
+        }
+        if (!confirmpass){
+            req.flash("msg", "A contra-senha é um campo obrigatório")
+            return res.redirect("/register")
+        }
+        if (password != confirmpass){
+            req.flash("msg", "As senhas devem ser iguais!")
+            return res.redirect("/register")
+        }
+
+        const checkUser = await User.findOne({email})
+        if (checkUser) {
+            req.flash("msg", "Dados já cadastrados")
+            return res.redirect("/register")
+        }
+
+        const salt = await bcrypt.genSalt(12)
+        const hashPass = await bcrypt.hash(password,salt)
+
+        try {
+           const user = new User({name, email, password:hashPass})
+           const newUser = await user.save()
+           const token = jwt.sign({
+            name: newUser.name,
+            id: newUser._id
+           }, process.env.JWT_SECRET)
+            
+            req.flash("msg", "Usuário registrado com sucesso!");
+            res.redirect("/login")
+        } catch (error) {
+            console.log(error)
         }
     }
 }
